1d56811705da93c4401e65952437ad45fca04d12
Palpatine <alvingitonga14@gmail.com>
2022-12-19 09:55:01 +0300

    Update JWTBruter.py

--- python Hacks/JWTBruter.py
+++ python Hacks/JWTBruter.py
@@ -9,7 +9,7 @@ with open(passwords) as secrets:
     for secret in secrets:
         try:
             payload = jwt.decode(encoded, secret.rstrip(), algorithms=['HS256'])
-            print('Token decodificado con la siguiente password ....[' + secret.rstrip() + ']')
+            print('Token decoded the password ....[' + secret.rstrip() + ']')
             break
         except jwt.InvalidTokenError as e:
             print(e)
