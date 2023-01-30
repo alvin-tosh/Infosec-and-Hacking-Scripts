48e1248c0a55af208f5e8b7a7d132b9a1b4a9697
Palpatine <alvingitonga14@gmail.com>
2023-01-30 08:59:59 +0300

    Add files via upload

---
+++ python Hacks/Openskybot/OpenskyBot.py
@@ -0,0 +1,55 @@
+import time
+import requests
+import tweepy
+
+# Replace these with your own API keys
+CONSUMER_KEY = '1fX8vmlE0F35SDkewjUuuFOir'
+CONSUMER_SECRET = 'XYJGtXs2ecK5mSi8jTUPUqh1DYRS3WlHDFduNyxG9LzVoINww8'
+ACCESS_TOKEN = '1610950751588696065-4ijosqVjRIWryJqSojnHBEvnsQLN4Y'
+ACCESS_TOKEN_SECRET = 'vZ6MZVez4bQ8ZMimLtw1BUqp7p4gCggWHmFcCUHRYfK44'
+
+# Authenticate with Twitter API
+auth = tweepy.OAuth1UserHandler(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+api = tweepy.API(auth)
+
+# Set the tail number to track
+tail_number = 'ZS-RMN'
+
+# Set the OpenSky API URL
+api_url = 'https://opensky-network.org/api/states/all'
+
+# Set the current position to None
+current_position = None
+
+while True:
+  # Make a request to the OpenSky API
+  response = requests.get(api_url)
+  
+  # Check if the request was successful
+  if response.status_code != 200:
+    # If the request failed, print an error message
+    print("Error making request to OpenSky API")
+  else:
+    # If the request was successful, get the response data
+    response_data = response.json()
+    
+    # Iterate through the list of states
+    for state in response_data['states']:
+      if state[1] == tail_number:
+        # If the tail number matches, get the latitude, longitude, and altitude
+        lat = state[6]
+        lon = state[5]
+        alt = state[7]
+
+        # Check if the position has changed
+        position = (lat, lon, alt)
+        if position != current_position:
+          # If the position has changed, tweet the new location of the airplane
+          message = f'Tail number {tail_number} is currently at latitude {lat}, longitude {lon}, and altitude {alt}'
+          api.update_status(status=message)
+
+          # Update the current position
+          current_position = position
+      
+  # Wait for 60 seconds before making another request
+  time.sleep(60)
